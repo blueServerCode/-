996e1c7795f63c35ebd5509b11a6ee31d6e597a2
blueServerCode <1223992357@qq.com>
2021-03-21 17:03:47 +0800

    first

--- script.js
+++ script.js
@@ -5,23 +5,24 @@ const currencyEl_two = document.getElementById("currency-two");
 const amountEl_two = document.getElementById("amount-two");
 
 const swap = document.getElementById("swap");
+const reload = document.getElementById("reload")
 const rateEl = document.getElementById("rate");
 
 // 通过fetch获取汇率并实现dom更新
 function calculate() {
-  const currency_one = currencyEl_one.value;
-  const currency_two = currencyEl_two.value;
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
 
-  //   console.log(currency_one, currency_two);
+    //   console.log(currency_one, currency_two);
 
-  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
-    .then(res => res.json())
-    .then(data => {
-      const rate = data.rates[currency_two];
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+        .then(res => res.json())
+        .then(data => {
+            const rate = data.rates[currency_two];
 
-      rateEl.innerText = `1${currency_one} = ${rate}${currency_two}`;
-      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
-    });
+            rateEl.innerText = `1${currency_one} = ${rate}${currency_two}`;
+            amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+        });
 }
 
 // 事件监听
@@ -31,10 +32,15 @@ currencyEl_two.addEventListener("change", calculate);
 amountEl_two.addEventListener("input", calculate);
 
 swap.addEventListener("click", () => {
-  const temp = currencyEl_one.value;
-  currencyEl_one.value = currencyEl_two.value;
-  currencyEl_two.value = temp;
-  calculate();
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
 });
+
+reload.addEventListener("click", () => {
+    window.location.reload()
+})
+
 
 calculate();
